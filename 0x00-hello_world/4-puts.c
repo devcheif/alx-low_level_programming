79589a2cc80ca4570ce6e3f43e368a635f2e2082
devcheif <viguobadia@gmail.com>
2022-04-11 06:36:08 -0700

    SE vfde

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* 
+ * main -Entry point
+ *
+ * return - Always 0 (sucess)
+ */
+
+char main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
