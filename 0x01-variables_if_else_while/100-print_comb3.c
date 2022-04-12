a08746c20cea7b32b5e64f8de5495ca655f0d0a1
devcheif <viguobadia@gmail.com>
2022-04-11 21:26:26 -0700

    lopkp

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 int number1;
 int number2;
 
-for (number1 = 48; number1 = 57; number1++)
+for (number1 = 48; number2 = 57; number1++)
 {
 putchar(number1);
 
-for (number2 = 48; number2 = 57; number2++)
+for (number2 = 48; number1 = 57; number2++)
 {
 putchar(number2);
 
