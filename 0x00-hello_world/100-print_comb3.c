ac176890dbf1960623b955534b3d91e1f6cfe22a
devcheif <viguobadia@gmail.com>
2022-04-11 20:11:05 -0700

    Starting toda

---
+++ 0x00-hello_world/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int digit1;
+int digit2;
+
+for (digit1 = 0; digit1 <= 9; digit++)
+{
+putchar(digit1);
+}
+
+for (digit2 =0; digit2 <= 9; digit2++)
+{
+putchar(digit2);
+}
+
+{
+putchar(",");
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
