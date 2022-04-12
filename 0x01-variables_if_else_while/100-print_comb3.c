99a9a0f12a5ab193ca7747af414b6d6817042ae1
devcheif <viguobadia@gmail.com>
2022-04-11 21:19:17 -0700

    lop

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int number1;
+int number2;
+
+for (number1 = 48; number1 = 57; number1++)
+{
+putchar(number1);
+
+for (number2 = 48; number2 = 57; number2++)
+{
+putchar(number2);
+
+if (number1, number2 == 57)
+
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+}
