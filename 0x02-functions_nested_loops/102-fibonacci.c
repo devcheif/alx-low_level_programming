43a8160bec9dbd180525859f91ef12b9571adcca
devcheif <viguobadia@gmail.com>
2022-04-13 15:51:31 -0700

    lovely

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,12 +13,12 @@ int n1 = 1;
 int n2 = 2;
 int n3;
 int i;
-int number = 98;
+int number = 50;
 
 printf("The number of the elements: %d\n", number);
 printf("%d, %d", n1, n2);
 
-for (i=3; i < number; ++i)
+for (i=3; i <= number; ++i)
 {
 n3 = n1 + n2;
 printf("%d", n3);
