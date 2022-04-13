963752c359e2b8e33df4f818ed0f9a97ad2cc251
devcheif <viguobadia@gmail.com>
2022-04-13 16:47:14 -0700

    love

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,17 +10,17 @@ int main(void)
 {
 int n1 = 1;
 int n2 = 2;
-int n3;
+int n3 = n1 +n2;
 int i;
 int number = 50;
 
 printf("The number of the elements: %d\n", number);
-printf("%d, %d", n1, n2);
+printf("%d, %d, ", n1, n2);
 
-for (i=3; i < number; ++i)
+for (i=3; i <= number; ++i)
 {
 n3 = n1 + n2;
-printf("%d", n3);
+printf("%d, ", n3);
 n1=n2;
 n2=n3;
 
