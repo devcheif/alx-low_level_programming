3faa4a7218d0c12f8228444567be89ca398bc7c8
devcheif <viguobadia@gmail.com>
2022-04-13 19:34:51 -0700

    settling mmmcq

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 1, b = 2, resul = a + b, i, num = 98;
+
+printf("The number of matches is: ");
+printf("%d, %d, ", a, b);
+
+for (i = 3; i <= num; i++)
+{
+printf("%d, " result);
+result = a + b;
+a = b;
+b = result;
+}
+
+return (0);
+}
