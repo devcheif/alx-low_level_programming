9bbbceb0a61e855ca26a279b0508c51bcc57d929
devcheif <viguobadia@gmail.com>
2022-04-20 16:52:39 -0700

    lone lover

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of the integer to 98
+ * @n: pointer to an integer to be updated
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+after Easter, we  resume alx school, im hoping bto lear and execute arrays and pointers. sorry just TYPO errors, an dim using ECHO not VI, VIM OR EMACS. I wish myuself all the freaking best. lob
+ve it
